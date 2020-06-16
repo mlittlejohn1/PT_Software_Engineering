abb0accea89b0d3743c4e8d8282a5de21bfc7975
Mekia Littlejohn <mekia@Mekias-MacBook-Pro.local>
2020-06-16 00:18:20 -0400

    took it easy

--- Mod1/week-3 /w3d3/answers.js
+++ Mod1/week-3 /w3d3/answers.js
@@ -1,3 +1,4 @@
+
 //Datatypes Refresher
 //For each of the following, write which datatypes you would use to represent the data, and then give a small example of the data structure:  
 
@@ -32,12 +33,38 @@ console.log(studentNames_Locations);
 //Data Structure: Array of objects with Array
 /*Data Structure:*/
 
-const studentNames_Locations = [
+const studentNames_Locations1 = [
     {name: 'Mekia', location: 'Columbus, Oh', favTvShows: ['The Resident', 'Power', 'Insecure']}, 
     {name: 'Chris', location: 'Columbus, Oh', favTvShows: ['The Simpsons', 'ER', 'Dave']},
     {name: 'Bruce', location: 'Columbus, Oh', favTvShows: ['Superman', 'Batman', 'Thor']}
 
 ]
-console. log(studentNames_Locations);
+console. log(studentNames_Locations1);
 
 
+///Take it easy//
+
+//1. Make an array that holds all of the colors of the rainbow.
+
+const rainbowColors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+console.log(rainbowColors);
+
+//2. Write code that will access "blue" from the rainbow array.
+
+console.log(rainbowColors [4]);
+
+//3. Make an object that is called your name and holds the information about your favorite food,Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype.
+     
+const mekia = {
+
+        favoriteFood: 'pasta',
+        hobby: 'family',
+        hometown: 'columbus',
+        favoriteDataType: 'undecided'
+} ;
+
+//4. Write code that will access your hobby from the object that you just created.
+
+    console.log(mekia.hobby);
+    
+   
